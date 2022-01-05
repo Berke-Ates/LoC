bb4ab76ced1c89d91501cb44a39b7842458eef71
Berke Ates <berke@ates.ch>
2022-01-05 10:36:15 +0100

    added fails

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 try {
     const folder = core.getInput('folder');
     const maxLines = core.getInput('max-lines');
+    let hasErr = false;
 
     file.walkSync(folder, function (start, dirs, names) {
         names.forEach(name => {
@@ -14,10 +15,15 @@ try {
             const LoC = data.split('\n').length;
 
             if (LoC > maxLines) {
-                console.error(`${path}: Too many LoC: ${LoC}`);
+                console.error(`Too many LoC(${LoC}): ${path}`);
+                hasErr = true;
             }
         });
     });
+
+    if (hasErr) {
+        throw "Some files have too many lines of code";
+    }
 
 } catch (error) {
     core.setFailed(error.message);
