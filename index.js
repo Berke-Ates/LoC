fa798596f0ba56c2357eff4b83f46980a5413110
Berke Ates <berke@ates.ch>
2022-01-05 16:41:37 +0100

    indentation

--- index.js
+++ index.js
@@ -4,32 +4,32 @@ const file = require('file');
 const fs = require('fs');
 
 try {
-    const folder = core.getInput('folder');
-    const exclude = core.getInput('exclude').split(' ');
-    const maxLines = core.getInput('max-lines');
-    let hasErr = false;
+  const folder = core.getInput('folder');
+  const exclude = core.getInput('exclude').split(' ');
+  const maxLines = core.getInput('max-lines');
+  let hasErr = false;
 
-    file.walkSync(folder, function (start, dirs, names) {
-        names.forEach(name => {
-            let skipFile = false;
-            exclude.forEach(ext => skipFile = skipFile || name.endsWith(ext));
-            if (!skipFile) {
-                const path = `${start}/${name}`;
-                const data = fs.readFileSync(path).toString();
-                const LoC = data.split('\n').length;
+  file.walkSync(folder, function (start, dirs, names) {
+    names.forEach(name => {
+      let skipFile = false;
+      exclude.forEach(ext => skipFile = skipFile || name.endsWith(ext));
+      if (!skipFile) {
+        const path = `${start}/${name}`;
+        const data = fs.readFileSync(path).toString();
+        const LoC = data.split('\n').length;
 
-                if (LoC > maxLines) {
-                    console.error(`Too many LoC(${LoC}): ${path}`);
-                    hasErr = true;
-                }
-            }
-        });
+        if (LoC > maxLines) {
+          console.error(`Too many LoC(${LoC}): ${path}`);
+          hasErr = true;
+        }
+      }
     });
+  });
 
-    if (hasErr) {
-        throw 'Some files have too many lines of code';
-    }
+  if (hasErr) {
+    throw 'Some files have too many lines of code';
+  }
 
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
